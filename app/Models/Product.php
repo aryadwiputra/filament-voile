8a1f9f0c52bf14ab86a2e1187aed9e516ce95727
Arya Dwi Putra <aryadptr.developer@gmail.com>
2025-01-01 23:27:39 +0700

    Create product migration and model

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'slug',
+        'price',
+        'thumbnail',
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function photos(){
+        return $this->hasMany(ProductPhoto::class);
+    }
+}
